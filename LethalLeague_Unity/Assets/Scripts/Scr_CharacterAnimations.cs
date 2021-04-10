9472220a01dde47aea07eb5eb677dd61135c835e
PÄRET Paul <paret.paul@orange.fr>
2021-04-11 01:38:35 +0200

    WallRide de ses morts

---
+++ LethalLeague_Unity/Assets/Scripts/Scr_CharacterAnimations.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Scr_CharacterMovement))]
+public class Scr_CharacterAnimations : MonoBehaviour
+{
+    [Header("Edit")]
+    [SerializeField, Range(0f, 10f)] private float fallAnimCorrection = 0f;
+
+    private Animator animator;
+    private Scr_CharacterMovement movement;
+
+    
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<Scr_CharacterMovement>();
+    }
+
+    private void Update()
+    {
+        MovementAnimation();
+    }
+
+    private void MovementAnimation()
+    {
+        if (movement.isIdle && movement.isGrounded)
+        {
+            Idle();
+        }
+        else if (movement.isRun && movement.isGrounded)
+        {
+            Run();
+        }
+        else if (movement.isWallRide)
+        {
+            WallRide();
+        }
+        else if (!movement.isGrounded)
+        {
+            Jump();
+        }
+        else if (movement.isCrouch)
+        {
+            Crouch();
+        }
+          
+    }
+
+    private void Idle()
+    {
+        if (movement.lastXVelocity > 0f)
+        {
+            animator.Play("ACl_PlayerIdle");
+        }
+        else if (movement.lastXVelocity < 0)
+        {
+            animator.Play("ACl_PlayerIdleFlip");
+        }
+        else
+        {
+            animator.Play("ACl_PlayerIdle");
+        }
+    }
+
+    private void Run()
+    {
+        if (movement.velocity.x > 0)
+        {
+            animator.Play("ACl_PlayerRun");
+        }
+        else
+        {
+            animator.Play("ACl_PlayerRunFlip");
+        }
+    }
+
+    private void Crouch()
+    {
+        if (movement.horizontalInput > 0)
+        {
+            animator.Play("ACl_PlayerCrouch");
+        }
+        else if (movement.horizontalInput < 0)
+        {
+            animator.Play("ACl_PlayerCrouchFlip");
+        }
+        else
+        {
+            if (movement.lastXVelocity > 0f)
+            {
+                animator.Play("ACl_PlayerCrouch");
+            }
+            else if (movement.lastXVelocity < 0f)
+            {
+                animator.Play("ACl_PlayerCrouchFlip");
+            }
+            else
+            {
+                animator.Play("ACl_PlayerCrouch");
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        if (movement.velocity.y > fallAnimCorrection)
+        {
+            if (movement.velocity.x > 0)
+            {
+                animator.Play("ACl_PlayerJump");
+            }
+            else if (movement.velocity.x < 0)
+            {
+                animator.Play("ACl_PlayerJumpFlip");
+            }
+            else
+            {
+                if (movement.lastXVelocity > 0f)
+                {
+                    animator.Play("ACl_PlayerJump");
+                }
+                else if (movement.lastXVelocity < 0f)
+                {
+                    animator.Play("ACl_PlayerJumpFlip");
+                }
+                else
+                {
+                    animator.Play("ACl_PlayerJump");
+                }
+            }
+        }
+        else
+        {
+            if (movement.velocity.x > 0)
+            {
+                animator.Play("ACl_PlayerFall");
+            }
+            else if (movement.velocity.x < 0)
+            {
+                animator.Play("ACl_PlayerFallFlip");
+            }
+            else
+            {
+                if (movement.lastXVelocity > 0f)
+                {
+                    animator.Play("ACl_PlayerFall");
+                }
+                else if (movement.lastXVelocity < 0f)
+                {
+                    animator.Play("ACl_PlayerFallFlip");
+                }
+                else
+                {
+                    animator.Play("ACl_PlayerFall");
+                }
+            }
+        }
+    }
+
+    private void WallRide()
+    {
+        if (movement.horizontalInput > 0)
+        {
+            animator.Play("ACl_PlayerWallRideFlip");
+        }
+        else
+        {
+            animator.Play("ACl_PlayerWallRide");
+        }
+    }
+}
--- LethalLeague_Unity/Assets/Scripts/Scr_CharacterMovement.cs
+++ LethalLeague_Unity/Assets/Scripts/Scr_CharacterMovement.cs
@@ -5,32 +5,46 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Scr_CharacterMovement : MonoBehaviour
 {
+    #region Variables
     [Header("Edit")]
     [SerializeField] private float speed = 9f;
+    [SerializeField] private float crouchMinDuration = 0.2f;
     [SerializeField] private float slideDeceleration = 70f;
     [SerializeField] private float jumpHeight = 4f;
+    [SerializeField] private Vector2 wallJumpForce = new Vector2(3f, 3f);
+    [SerializeField] private float wallJumpDuration = 0.5f;
     [SerializeField] private float gravityScale = 1f;
-    [SerializeField] private float lowJumpGravityModifier = 1f;
+    [SerializeField] private float highJumpGravityModifier = 1f;
     [SerializeField] private float fallGravityModifier = 1f;
     [SerializeField] private float forceFallGravityModifier = 1f;
+    [SerializeField] private float wallRideGravityModifier = 1f;
+    [SerializeField, Range(0f, -10f)] private float fallBehaviorCorrection = 0f;
 
     [Header("Inputs")]
-    [SerializeField] private float horizontalInput;
+    public float horizontalInput;
     [SerializeField] private bool jumpInput;
     [SerializeField] private float verticalInput;
 
     [Header("States")]
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private bool isJumping;
-    [SerializeField] private bool isFalling;
-    [SerializeField] private bool isIdle;
-    [SerializeField] private bool isRun;
-    [SerializeField] private bool isCrouch;
-    [SerializeField] private bool isWallRide;
+    public bool isGrounded;
+    public bool isFalling;
+    public bool isIdle;
+    public bool isRun;
+    public bool isCrouch;
+    public bool isWallRide;
+    public bool isWallJump;
 
 
+    [HideInInspector] public float lastXVelocity = 0f;
     private BoxCollider2D boxCollider;
-    private Vector2 velocity;
+    [HideInInspector] public Vector2 velocity;
+    private bool lockGravity = false;
+    private bool lockCrouch = false;
+    private bool canCrouch = true;
+    private float currentCrouchTime = 0f;
+    private float currentWallJumpTime = 0f;
+    private float lastHorizontalInput = 0f;
+    #endregion
 
     private void Awake()
     {
@@ -86,9 +100,10 @@ public class Scr_CharacterMovement : MonoBehaviour
 
     private void Idle()
     {
-        if (!isRun && !isCrouch && !isWallRide && !isJumping && !isFalling)
+        if (!isRun && !isCrouch && !isWallRide && !isWallJump && !isFalling)
         {
             isIdle = true;
+            velocity.x = 0f;
         }
         else
         {
@@ -98,10 +113,11 @@ public class Scr_CharacterMovement : MonoBehaviour
 
     private void HorizontalMovement()
     {
-        if (!isCrouch /*&& horizontalInput != 0*/)
+        if (!isCrouch && !isWallRide && !isWallJump && horizontalInput != 0)
         {
             isRun = true;
             velocity.x = speed * horizontalInput;
+            lastXVelocity = velocity.x;
         }
         else
         {
@@ -111,56 +127,109 @@ public class Scr_CharacterMovement : MonoBehaviour
 
     private void Crouch()
     {
-        if (isGrounded && verticalInput == -1f)
+        if (!canCrouch && verticalInput != -1)
+        {
+            canCrouch = true;
+        }
+
+        if ((isGrounded && verticalInput == -1f && canCrouch) || lockCrouch)
         {
             isCrouch = true;
+            lockCrouch = true;
+
+            currentCrouchTime += Time.deltaTime;
+
+            if(currentCrouchTime >= crouchMinDuration)
+            {
+                lockCrouch = false;
+            }
 
             velocity.x = Mathf.MoveTowards(velocity.x, 0, slideDeceleration * Time.deltaTime);
         }
         else
         {
+            currentCrouchTime = 0f;
             isCrouch = false;
         }
     }
 
     private void Jump()
     {
-        if (isGrounded && !isCrouch)
+        if (isGrounded)
         {
             velocity.y = 0;
 
-            if (jumpInput)
+            if (jumpInput && !isCrouch)
             {
                 isGrounded = false;
                 velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y * gravityScale));
             }
         }
+        else if ((isWallRide && jumpInput) || isWallJump)
+        {
+            if (!isWallJump)
+            {
+                lastHorizontalInput = horizontalInput;
+                isWallJump = true;
+                isWallRide = false;
+            }
+
+            currentWallJumpTime += Time.deltaTime;
+
+            if (currentWallJumpTime >= wallJumpDuration)
+            {
+                isWallJump = false;
+                currentWallJumpTime = 0f;
+            }
+
+            velocity.y = Mathf.Sqrt(2 * wallJumpForce.y * Mathf.Abs(Physics2D.gravity.y * gravityScale));
+            velocity.x = wallJumpForce.x * -lastHorizontalInput;
+        }
 
         if (!isGrounded)
         {
-            float gravityModifier;
-
-            if (velocity.y < 0)
-            {
-                gravityModifier = fallGravityModifier;
-            }
-            else if (velocity.y > 0 && !jumpInput)
-            {
-                gravityModifier = lowJumpGravityModifier;
-            }
-            else
-            {
-                gravityModifier = 1f;
-            }
-
-            if (verticalInput == -1f)
-            {
-                gravityModifier = forceFallGravityModifier;
-            }
-
-            velocity.y += Physics2D.gravity.y * gravityScale * gravityModifier * Time.deltaTime;
+            Gravity();
         }
 
+    }
+
+    private void Gravity()
+    {
+        if (velocity.y > fallBehaviorCorrection)
+        {
+            isFalling = false;
+        }
+        else
+        {
+            isFalling = true;
+        }
+
+        float gravityModifier;
+
+        if (isWallRide && !lockGravity)
+        {
+            gravityModifier = wallRideGravityModifier;
+        }
+        else if ((verticalInput == -1f && horizontalInput == 0 && isFalling) || lockGravity)
+        {
+            gravityModifier = forceFallGravityModifier;
+            lockGravity = true;
+            canCrouch = false;
+        }
+        else if (isFalling)
+        {
+            gravityModifier = fallGravityModifier;
+        }
+        else if (!isFalling && jumpInput)
+        {
+            gravityModifier = highJumpGravityModifier;
+        }
+        else
+        {
+            gravityModifier = 1f;
+        }
+
+        velocity.y += Physics2D.gravity.y * gravityScale * gravityModifier * Time.deltaTime;
     }
 
     private void CollisionDetection()
@@ -180,19 +249,32 @@ public class Scr_CharacterMovement : MonoBehaviour
             {
                 transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
 
-                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && velocity.y < 0)
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) == 0 && velocity.y < 0)
                 {
+                    velocity.y = 0;
+
                     isGrounded = true;
+                    isFalling = false;
+                    lockGravity = false;
+
                 }
 
-                if (((Vector2.Angle(colliderDistance.normal, Vector2.right) < 90 && horizontalInput == -1) || (Vector2.Angle(colliderDistance.normal, Vector2.left) < 90 && horizontalInput == 1)) && !isGrounded)
+                if (((Vector2.Angle(colliderDistance.normal, Vector2.right) == 0 && horizontalInput == -1) || (Vector2.Angle(colliderDistance.normal, Vector2.left) == 0 && horizontalInput == 1)) && !isGrounded && velocity.y < 0)
                 {
+                    if (!isWallRide)
+                    {
+                        velocity = Vector2.zero;
+                    }
+
                     isWallRide = true;
+                    
                 }
                 else
                 {
                     isWallRide = false;
                 }
+
+
             }
         }
     }
